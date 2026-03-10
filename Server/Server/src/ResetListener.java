6e0538ae998b46ef9a6f05559fa6cd9eb3f78d42
shadow <68437626+zahzag@users.noreply.github.com>
2026-03-10 11:09:25 +0000

    cleaning

--- Server/Server/src/ResetListener.java
+++ Server/Server/src/ResetListener.java
@@ -16,24 +16,12 @@ public class ResetListener implements Runnable {
 	private static final Logger log = LoggerFactory.getLogger(ResetListener.class);
 	private DatagramSocket resetSocket;
 	private int resetPort = 9950;
-	//private int resetPort = 9920;
 	private byte[] resetBuffer;
 	private DatagramPacket resetPacket;
 	private int state;
-	//private String ipaddr = "10.0.0.2";
-	private String ipaddr = "127.0.0.1";
+	private String ipaddr = "10.0.0.2";
+	public static long arrivalTime=0;
 
-	/*
-	*public ResetListener() {
-			try {
-				resetSocket = new DatagramSocket(resetPort);
-			} catch (SocketException e) {
-				e.printStackTrace();
-			}
-			this.state = Server.RUN;
-		}
-	*
-	* */
 	public ResetListener() {
 		try {
 			resetSocket = new DatagramSocket(resetPort, InetAddress.getByName(ipaddr));
@@ -44,6 +32,14 @@ public class ResetListener implements Runnable {
         }
         this.state = Server.RUN;
 	}
+	
+	private CustomWorkerThreadPool awaitExecutor() {
+        CustomWorkerThreadPool exec;
+        while ((exec = WorkerThreadPool.executorPool) == null) {
+            try { Thread.sleep(50); } catch (InterruptedException ie) { Thread.currentThread().interrupt(); return null; }
+        }
+        return exec;
+    }
 
 	public void run() {
 		System.out.println("ResetListener running...");
@@ -55,7 +51,6 @@ public class ResetListener implements Runnable {
 
 			try {
 				resetSocket.receive(resetPacket);
-				//System.out.println("packet recieved");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -64,24 +59,19 @@ public class ResetListener implements Runnable {
 			byte[] packetContents = new byte[packetLen];
 			System.arraycopy(resetBuffer, offSet, packetContents, 0, packetLen);
 			String content = new String(packetContents);
-			//System.out.println("Packet Received in ResetListener: " + content);
+			
+			CustomWorkerThreadPool exec = awaitExecutor();
+            if (exec == null) return;
 
 			if (content.equals("RESET1")) {
 				System.out.println("RESET JOB 1 ADDED in the start of the queue");
-//				Server.enterSystem.add((long) 0);
-				WorkerThreadPool.executorPool.execute(new ResetJob(System.nanoTime(), 0, true));
-				int i=0;
-				while(WorkerThreadPool.executorPool == null && i<10){
-					log.info("WorkerThreadPool is null");
-					WorkerThreadPool.executorPool.execute(new ResetJob(System.nanoTime(), 0, true));
-					i++;
-				}
+
+				exec.execute(new ResetJob(System.nanoTime(), 0, true));
 
 			} else {
 
 				System.out.println("RESET JOB 2 ADDED in the end of the queue");
-//				Server.enterSystem.add((long) 0);
-				WorkerThreadPool.executorPool.execute(new ResetJob(System.nanoTime(), 0, false));
+				exec.execute(new ResetJob(System.nanoTime(), 0, false));				
 				int i=0;
 				while(WorkerThreadPool.executorPool == null && i<10){
 					log.info("WorkerThreadPool is null");
