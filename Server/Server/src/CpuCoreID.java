cc43edf69b12c2be4cdb7d5975960a8882e5ed36
shadow <68437626+zahzag@users.noreply.github.com>
2026-03-10 10:15:58 +0000

    cleaning

--- Server/Server/src/CpuCoreID.java
+++ Server/Server/src/CpuCoreID.java
@@ -9,6 +9,8 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public class CpuCoreID {
+    public CpuCoreID() {
+       }
     public interface CLibrary extends Library {
         CLibrary INSTANCE= (CLibrary) Native.loadLibrary("c",CLibrary.class);
         int sched_getcpu();
